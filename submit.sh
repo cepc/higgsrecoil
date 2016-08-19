1e293db3f71a48685c3b2f4c675c221ccde5bc7c
SHI Xin <shixin@ihep.ac.cn>
2016-08-19 13:57:36 +0800

    add bkg and plot

--- submit.sh
+++ submit.sh
@@ -10,6 +10,8 @@ usage() {
     printf "\n\t%-5s\n" "./submit.sh [OPTION]" 
     printf "\nOPTIONS\n" 
     printf "\n\t%-5s  %-40s\n"  "0.1.1"    "Run on signal samples" 
+    printf "\n\t%-5s  %-40s\n"  "0.1.2"    "Run on background samples" 
+    printf "\n\t%-5s  %-40s\n"  "0.1.3"    "Draw plots of signal and background" 
     printf "\nDATE\n"
     printf "\n\t%-5s\n" "AUGUST 2016"     
 }
@@ -28,6 +30,18 @@ case $option in
 	   export MARLIN_DLL=./lib/libHiggsRecoilMass.so
 	   mkdir result 
 	   Marlin steer/signal.steer 
+	   ;;
+
+    0.1.2) echo "Running on background sample..."
+	   unset MARLIN_DLL
+	   export MARLIN_DLL=./lib/libHiggsRecoilMass.so
+	   mkdir result 
+	   Marlin steer/bkg.steer 
+	   ;;
+
+    0.1.3) echo "Draw plots with signal and background..."
+	   mkdir plot 
+	   root -l script/draw_mrecoil.C 
 	   ;;
    
 esac
